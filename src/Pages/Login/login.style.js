b427519c496674a84f758d2188c34aee64aef2e4
PathakDeepak108 <85241413+DeepakPathak18@users.noreply.github.com>
2021-06-29 11:31:02 +0530

    Login Module

---
+++ src/Pages/Login/login.style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const LoginContent = styled.div`
+    border: 1px solid;
+    text-align: center;
+    height: 300px;
+    font-family: "Helvetica";
+    font-size: 18px;
+    width: 30%;
+    vertical-align: middle;
+    margin: auto;
+    background-color:  #ADD8E6;
+    color: brown;
+    border-color: #ADD8E6;
+    margin-top: 100px;
+
+    & > htmlFor {
+    }
+
+    & > button {
+        color: brown;
+        padding: 10px 20px 10px 20px;
+        border-radius: 3px;
+    }
+    & > form {
+        top: 50%;
+        left: 50%;
+        position  : absolute;
+        transform : translate(-50%, -50%);
+    }
+`;
