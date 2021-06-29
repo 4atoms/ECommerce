c7bf752379cd9ce5e9caaa26e753bb8bef569949
PathakDeepak108 <85241413+DeepakPathak18@users.noreply.github.com>
2021-06-29 11:02:01 +0530

    Not Found File Added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom';
+import Login from "../src/Pages/Login";
+//import Application from "../src/Pages/Application";
 
 ReactDOM.render(
-    <div>Hello World!</div>,
+    <Login />,
+    //<Application />,
     // <div className="text"><Hello/></div>,
     document.getElementById("app-root")
 );
