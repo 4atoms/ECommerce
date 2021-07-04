705c49ec9392953b53acfef82fc2bc786cf68c35
Manish Kumar <manishkumar222mani@gmail.com>
2021-07-04 15:37:01 +0530

    change in new index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import ReactDOM from 'react-dom';
-import Main from './Pages/Application/index';
+import React from "react";
+import ReactDOM from "react-dom";
+//import Main from './Pages/Application/index';
+import Main from "./Pages/Application";
 import Login from "../src/Pages/Login";
+//import Footer from "./Pages/Footer";
 
 ReactDOM.render(
     <div>
